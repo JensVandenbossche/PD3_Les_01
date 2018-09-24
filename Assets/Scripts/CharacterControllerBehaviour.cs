56f9d7a1418fad51505a26397aae4516c6379dc3
JensVandenbossche <43540790+JensVandenbossche@users.noreply.github.com>
2018-09-24 16:36:56 +0200

    Setup Dependancy between Behaviour and component for CharacterController
    Changes in haractorControllerBehaviour script

--- Assets/Scripts/CharacterControllerBehaviour.cs
+++ Assets/Scripts/CharacterControllerBehaviour.cs
@@ -1,16 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Assertions;
+
+//require components, best ook een klasse met alle scripts die op een component moeten
+//[RequireComponent(typeof(CharacterController))]
 
 public class CharacterControllerBehaviour : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    private CharacterController _characterController;
+
+	void Start ()
+    {
+        _characterController = GetComponent<CharacterController>(); //meestal deze gebruiken
+
+#if DEBUG
+        Assert.IsNotNull(_characterController, "DEPENCANDY ERROR: CharacterControllerBehaviour needs a CharacterController Component");
+
+        //if (_characterController == null)
+        //{
+        //    Debug.LogError("DEPENDANCY ERROR: CharacterControllerBehaviour needs a CharacterController Component");
+        //}
+#endif
+    }
 	
-	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
 		
 	}
 }
